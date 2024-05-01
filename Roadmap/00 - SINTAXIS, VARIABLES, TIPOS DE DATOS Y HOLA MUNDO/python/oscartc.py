4f0c19ab69f5e132d43047ad02e4f03243324ff9
Oscar <tc.oscar@gmail.com>
2024-04-30 20:48:17 -0500

    Create oscartc.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/oscartc.py
@@ -0,0 +1,18 @@
+# https://www.python.org/
+
+"""
+Esto también es
+un comentario
+en varias líneas
+"""
+
+'''
+Esto también es 
+un comentario 
+en varias líneas
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante"    # Por convención
